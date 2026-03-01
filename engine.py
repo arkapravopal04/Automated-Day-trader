b72670621f9813b5ed2a407da758f6f8388e3177
arkapravopal04 <arkapravopal04@gmail.com>
2026-03-01 21:56:16 +0530

    Changed the __add__ method of the Module class to include parameters from sub-modules. This allows for better organization of parameters when using nested modules. also added a new file module.py which contains the implementation of the Module class. This class is a base class for all neural network modules and provides a method to retrieve all parameters of the module, including those from sub-modules.

--- engine.py
+++ engine.py
@@ -7,27 +7,55 @@ class Tensor:
         self._prev = set(_children)
         self._op = _op 
         self.label = label
+    def matmul(self, other):
+        other = other if isinstance(other , Tensor) else Tensor(other)
+        out = Tensor(self.data @ other.data, (self, other), '@')
+        def _backward():
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
+        out._backward = _backward
+        return out
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
-        
         def _backward():
-            self.grad += 1.0 * out.grad
-            other.grad += 1.0 * out.grad
+            self_grad = out.grad
+            other_grad = out.grad
+            
+            while self_grad.ndim > self.data.ndim:
+                self_grad = self_grad.sum(axis=0)
+            for i, dim in enumerate(self.data.shape):
+                if dim == 1:
+                    self_grad = self_grad.sum(axis=i, keepdims=True)
+                    
+            self.grad += self_grad
+            other_grad = out.grad
+            while other_grad.ndim > other.data.ndim:
+                other_grad = other_grad.sum(axis=0)
+            for i, dim in enumerate(other.data.shape):
+                if dim == 1:
+                    other_grad = other_grad.sum(axis=i, keepdims=True)
+            other.grad += other_grad
         out._backward = _backward
         return out
-    def __mul__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)
-        out = Tensor(self.data * other.data, (self, other), '*')
-        def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-        out._backward = _backward
-        return out
-    def __relu__(self):
-        out = Tensor(0 if self.data < 0 else self.data, (self,), 'ReLU')
+    def relu(self):
+        out = Tensor(np.maximum(0, self.data), (self,), 'ReLU')
         def _backward():
             self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+        return out
+    def tanh(self):
+        t = np.tanh(self.data)
+        out = Tensor(t, (self,), 'tanh')
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+        return out
+    def sigmoid(self):
+        s = 1 / (1 + np.exp(-self.data))
+        out = Tensor(s, (self,), 'sigmoid')
+        def _backward():
+            self.grad += s * (1 - s) * out.grad
         out._backward = _backward
         return out
     def __pow__(self , other):
@@ -45,9 +73,23 @@ class Tensor:
             other.grad += (-self.data / (other.data**2)) * out.grad
         out._backward = _backward
         return out
-    def __rneg__(self):
+    def __neg__(self):
         return self * -1
     def __rmul__(self, other):
         return self * other
     def __radd__(self, other):
         return self + other
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
