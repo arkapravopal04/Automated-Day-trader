bf3b464c6d205d2bef068eb3f96db2a77f92e8ae
arkapravopal04 <arkapravopal04@gmail.com>
2026-03-03 10:44:49 +0530

    added im2col in the engine, made new script to house all the neural nets

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ class Tensor:
         def _backward():
             self.grad += out.grad @ other.data.T
             other.grad += self.data.T @ out.grad
-        out._backward = _backward
+        out._backward  = _backward
         return out
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
@@ -36,6 +36,14 @@ class Tensor:
                 if dim == 1:
                     other_grad = other_grad.sum(axis=i, keepdims=True)
             other.grad += other_grad
+        out._backward = _backward
+        return out
+    def __mul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backward
         return out
     def relu(self):
@@ -73,12 +81,61 @@ class Tensor:
             other.grad += (-self.data / (other.data**2)) * out.grad
         out._backward = _backward
         return out
+    def __sub__(self, other): 
+        return self + (-other)
+    def __rsub__(self, other):
+        return other + (-self)
     def __neg__(self):
         return self * -1
     def __rmul__(self, other):
         return self * other
     def __radd__(self, other):
         return self + other
+    def sum(self):
+        out = Tensor(np.sum(self.data), (self,), 'sum')
+        def _backward():
+            self.grad += np.ones_like(self.data) * out.grad
+        out._backward = _backward
+        return out
+
+    def im2col(self, kernel_size, stride= 1, padding=0):
+        C, H, W = self.data.shape
+        KH, KW = kernel_size
+        out_h = (H + 2 * padding - KH) // stride + 1
+        out_w = (W + 2 * padding - KW) // stride + 1
+        img = np.pad(self.data, ((0, 0), (padding, padding), (padding, padding)), mode='constant')
+        cols = np.zeros((C * KH * KW, out_h * out_w))
+        for y in range(KH):
+            y_max = y + stride * out_h
+            for x in range(KW):
+                x_max = x + stride * out_w
+                cols[y*KW+x::KH*KW, :] = img[:, y:y_max:stride, x:x_max:stride].reshape(C, -1)
+        out = Tensor(cols, (self,), 'im2col')
+        def _backward():
+            dcols = out.grad
+            dimg = np.zeros_like(img)
+            for y in range(KH):
+                y_max = y + stride * out_h
+                for x in range(KW):
+                    x_max = x + stride * out_w
+                    dimg[:, y:y_max:stride, x:x_max:stride] += dcols[y*KW+x::KH*KW, :].reshape(C, out_h, out_w)
+            self.grad += dimg[:, padding:H+padding, padding:W+padding]
+        out._backward = _backward
+        return out
+    
+    def reshape(self, *shape):
+        out = Tensor(self.data.reshape(*shape), (self,), 'reshape')
+        def _backward():
+            self.grad += out.grad.reshape(self.data.shape)
+        out._backward = _backward
+        return out
+    
+    def transpose(self, axes):
+        out = Tensor(self.data.transpose(axes), (self,), 'transpose')
+        def _backward():
+            self.grad += out.grad.transpose(np.argsort(axes))
+        out._backward = _backward
+        return out
     
     def backward(self):
         topo = []
